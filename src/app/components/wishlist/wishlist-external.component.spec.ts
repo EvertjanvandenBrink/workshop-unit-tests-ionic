7ed597361ed4171b412e2ae7cd72f19849fb686e
Niels <N.vanLeeuwen1@student.han.nl>
2019-02-20 12:14:33 +0100

    component DOM testing

--- src/app/components/wishlist/wishlist-external.component.spec.ts
+++ src/app/components/wishlist/wishlist-external.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { WishlistComponent } from './wishlist.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {Product} from '../../classes/product';
 
 describe('WishlistComponent', () => {
     let component: WishlistComponent;
