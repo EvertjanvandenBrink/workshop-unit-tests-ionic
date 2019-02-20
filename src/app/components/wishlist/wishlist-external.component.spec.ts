f002f251c00735254172f608b599d7583eeb81a0
Niels <N.vanLeeuwen1@student.han.nl>
2019-02-20 12:13:36 +0100

    component DOM testing

---
+++ src/app/components/wishlist/wishlist-external.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { WishlistComponent } from './wishlist.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {Product} from '../../classes/product';
+
+describe('WishlistComponent', () => {
+    let component: WishlistComponent;
+    let fixture: ComponentFixture<WishlistComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ WishlistComponent ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WishlistComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeDefined();
+    });
+
+    it('should contain title "Wishlist"', () => {
+        // Here comes your first component test.
+    });
+});
