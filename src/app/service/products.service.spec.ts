dae6cb550bcb9612c648a021da32741f16cd84bd
Rick Goemans <rickgoemans@gmail.com>
2019-02-21 10:53:43 +0100

    Removed test so we can code it together in the demonstration

--- src/app/service/products.service.spec.ts
+++ src/app/service/products.service.spec.ts
@@ -1,8 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsService } from './products.service';
-import { Product } from '../classes/product';
-import { products } from '../mock-data/products';
 
 describe('ProductsService', () => {
     let service: ProductsService;
@@ -18,17 +16,11 @@ describe('ProductsService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('#getWishlist should return wishlist from observable', (done: DoneFn) => {
-        service.getWishlist().subscribe((wishlist: Array<Product>) => {
-            expect(wishlist).toEqual([]);
-            done();
-        });
+    it('#getWishlist should return wishlist from observable', () => {
+        // Here comes you first service test
     });
 
-    it('#getProducts should return products from observable', (done: DoneFn) => {
-        service.getProducts().subscribe((retrievedProducts: Array<Product>) => {
-            expect(retrievedProducts).toEqual(products);
-            done();
-        });
+    it('#getProducts should return products from observable', () => {
+        // Here comes your second service test
     });
 });
